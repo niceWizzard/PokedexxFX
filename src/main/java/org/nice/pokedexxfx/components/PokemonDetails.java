f848882a115ffb04eff616a3d61cf3622edf2440
Richard <richard232manansala@gmail.com>
2024-05-07 09:19:45 +0800

    Added some placeholder current pokemon name in PokemonDetails

--- src/main/java/org/nice/pokedexxfx/components/PokemonDetails.java
+++ src/main/java/org/nice/pokedexxfx/components/PokemonDetails.java
@@ -2,9 +2,14 @@ package org.nice.pokedexxfx.components;
 
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import org.nice.pokedexxfx.services.PokemonService;
 
 public class PokemonDetails extends GridPane {
     public PokemonDetails() {
-        getChildren().add(new Text("Pokemon Details"));
+        var name = new Text("NAme");
+        getChildren().add(name);
+        PokemonService.getInstance().onCurrentPokemon().subscribe(v -> {
+           name.setText(v.name());
+        });
     }
 }
