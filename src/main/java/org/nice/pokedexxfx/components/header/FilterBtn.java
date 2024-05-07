a715816d08b3cc218898ef4b7d90ced371f1a0f1
Richard <richard232manansala@gmail.com>
2024-05-07 10:02:31 +0800

    Added working filter btn

--- src/main/java/org/nice/pokedexxfx/components/header/FilterBtn.java
+++ src/main/java/org/nice/pokedexxfx/components/header/FilterBtn.java
@@ -1,9 +1,60 @@
 package org.nice.pokedexxfx.components.header;
 
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import org.nice.pokedexxfx.models.PokemonType;
 
-public class FilterBtn extends Button {
+import org.nice.pokedexxfx.services.SearchService;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FilterBtn extends MenuBar {
     public FilterBtn() {
-        setText("Filter");
+        Menu menu = new Menu("Filter");
+        var btnMap = new HashMap<String, CheckBox>();
+
+        var unselectAll = new Button("Unselect All");
+        unselectAll.setStyle("-fx-text-fill: black;");
+        var i = new CustomMenuItem(unselectAll);
+        i.setHideOnClick(false);
+        menu.getItems().add(i);
+
+        for (PokemonType value : PokemonType.values()) {
+            var checkbox = new CheckBox(value.name());
+            checkbox.setStyle("-fx-text-fill: black;");
+            var item = new CustomMenuItem(checkbox);
+            item.setHideOnClick(false);
+            menu.getItems().add(item);
+            btnMap.put(value.name(), checkbox);
+            checkbox.selectedProperty().addListener(v -> {
+                var list = new ArrayList<>(SearchService.getInstance().currentTypeFilters());
+                if(checkbox.isSelected()) {
+                    list.add(value);
+                    if(list.size() > 2) {
+                        list.removeFirst();
+                    }
+                } else {
+                    list.remove(value);
+                }
+                SearchService.getInstance().setTypeFilters(
+                        list
+                );
+            });
+        }
+        getMenus().add(menu);
+
+        SearchService.getInstance().onTypeFilterChange().subscribe(typeList -> {
+            btnMap.keySet().forEach(type -> {
+                var btn = btnMap.get(type);
+                btn.setSelected(typeList.contains(PokemonType.valueOf(type)));
+            });
+        });
+
+        unselectAll.setOnMouseClicked(v -> {
+            SearchService.getInstance().setTypeFilters(List.of());
+        });
+
     }
 }
