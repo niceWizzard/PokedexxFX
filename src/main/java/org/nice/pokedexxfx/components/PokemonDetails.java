2f26855561445a8dcf6cfcf961b4237a0ddbb557
LeinnarF <hilariofranniel@gmail.com>
2024-05-16 15:09:20 +0800

    Details and list WIP

--- src/main/java/org/nice/pokedexxfx/components/PokemonDetails.java
+++ src/main/java/org/nice/pokedexxfx/components/PokemonDetails.java
@@ -1,15 +1,175 @@
 package org.nice.pokedexxfx.components;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+
+import java.util.List;
+
+import org.nice.pokedexxfx.models.PokemonModel.Ability;
+import org.nice.pokedexxfx.services.PokemonImage;
 import org.nice.pokedexxfx.services.PokemonService;
 
 public class PokemonDetails extends GridPane {
+
+    String Name, Height, Weight, Species, Gender;
+    int ID;
+    List<String> Type;
+    List<Ability> Ability;
+    String zeros = "00";
+
+    Label pokemonID, pokemonName, pokemonSpecies, pokeH, pokeW, pokeG, pokeA, type1Label, type2Label;
+    ImageView pokeImageView;
+    Image image;
+
+    private void updateLabel(Label label, String text) {
+        if (label != null) {
+            label.setText(text);
+        }
+    }
+
     public PokemonDetails() {
-        var name = new Text("NAme");
-        getChildren().add(name);
-        PokemonService.getInstance().onCurrentPokemon().subscribe(v -> {
-           name.setText(v.name());
+
+        PokemonService.getInstance().onCurrentPokemon().subscribe(p -> {
+            Name = p.name();
+            updateLabel(pokemonName, Name);
+
+            Species = p.species();
+            updateLabel(pokemonSpecies, Species);
+            ID = p.id();
+            if (ID < 10) {
+                zeros = "00";
+            } else if (ID < 100) {
+                zeros = "0";
+            } else {
+                zeros = "";
+            }
+            if (pokemonID != null) {
+                pokemonID.setText(zeros + ID);
+            }
+            Height = p.profile().height();
+            updateLabel(pokeH, Height);
+            Weight = p.profile().weight();
+            updateLabel(pokeW, Weight);
+            Gender = p.profile().gender();
+            if (p.profile().gender().equals("Genderless")) {
+                Gender = "genderless";
+            } else if (p.profile().gender().equals("100:0")) {
+                Gender = "♂";
+            } else if (p.profile().gender().equals("0:100")) {
+                Gender = "♀";
+            } else {
+                Gender = "♂ ♀";
+            }
+            updateLabel(pokeG, Gender);
+            Ability = p.profile().ability();
+            if (pokeA != null) {
+                pokeA.setText("Abilities: " + (Ability.isEmpty() ? "None" : Ability.get(0).name()));
+            }
+            image = PokemonImage.getHires(p);
+            if (pokeImageView != null) {
+                pokeImageView.setImage(image);
+            }
         });
+
+        // Image Section
+        pokeImageView = new ImageView(image);
+        pokeImageView.setFitHeight(200);
+        pokeImageView.setFitWidth(200);
+        pokeImageView.setPreserveRatio(true);
+
+        VBox pokeImagecontainer = new VBox();
+        pokeImagecontainer
+                .setStyle(
+                        "-fx-background-color: #E9FFFB; -fx-border-color: #808080; -fx-border-width: 15px;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.3), 5, 0, 0, 5);");
+        pokeImagecontainer.setAlignment(Pos.CENTER);
+        pokeImagecontainer.getChildren().add(pokeImageView);
+
+        add(pokeImagecontainer, 0, 0);
+
+        // Basic Info Section
+        VBox pokeBasic = new VBox();
+        pokeBasic.setSpacing(5);
+        pokeBasic.setPadding(new Insets(10));
+
+        pokemonID = new Label(zeros + ID);
+        pokemonID.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        pokeBasic.getChildren().add(pokemonID);
+
+        pokemonName = new Label(Name);
+        pokemonName.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+        pokeBasic.getChildren().add(pokemonName);
+
+        pokemonSpecies = new Label(Species);
+        pokemonSpecies.setFont(Font.font("Verdana", 16));
+        pokemonSpecies.setTextFill(Color.GRAY);
+        pokeBasic.getChildren().add(pokemonSpecies);
+
+        // Stats Section
+        VBox pokeStats = new VBox();
+        pokeStats.setSpacing(5);
+        pokeStats.setPadding(new Insets(10));
+        pokeStats.setStyle(
+                "-fx-background-color: #FFF3C7; -fx-background-radius: 20px; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.3), 5, 0, 0, 5);");
+
+        pokeH = new Label("Height: " + Height);
+        pokeH.setFont(Font.font("Courier", 16));
+        pokeStats.getChildren().add(pokeH);
+
+        pokeW = new Label("Weight: " + Weight);
+        pokeW.setFont(Font.font("Courier", 16));
+        pokeStats.getChildren().add(pokeW);
+
+        pokeG = new Label("Gender: " + Gender);
+        pokeG.setFont(Font.font("Courier", 16));
+        pokeStats.getChildren().add(pokeG);
+
+        pokeA = new Label("Abilities: " + (Ability.isEmpty() ? "None" : Ability.get(0).name()));
+        pokeA.setFont(Font.font("Courier", 16));
+        pokeStats.getChildren().add(pokeA);
+
+        // Type Section
+        // HBox typePanel = new HBox();
+        // typePanel.setSpacing(10);
+        // typePanel.setPadding(new Insets(10));
+
+        // type1Label = new Label(Type.get(0));
+        // type1Label.setFont(Font.font("Arial", 14));
+        // type1Label.setTextFill(Color.WHITE);
+
+        // type2Label = new Label();
+        // if (Type.size() > 1) {
+        // type2Label.setText(Type.get(1));
+        // type2Label.setFont(Font.font("Arial", 14));
+        // type2Label.setTextFill(Color.WHITE);
+        // }
+
+        // Format
+        VBox rightSide = new VBox();
+        rightSide.setSpacing(10);
+        rightSide.getChildren().addAll(pokeBasic, pokeStats);
+
+        VBox.setVgrow(pokeBasic, Priority.ALWAYS);
+        VBox.setVgrow(pokeStats, Priority.ALWAYS);
+        add(rightSide, 1, 0);
+
+        GridPane.setHgrow(rightSide, Priority.ALWAYS);
+        GridPane.setVgrow(rightSide, Priority.ALWAYS);
+        GridPane.setHgrow(pokeImagecontainer, Priority.ALWAYS);
+        GridPane.setVgrow(pokeImagecontainer, Priority.ALWAYS);
+
+        GridPane.setMargin(rightSide, new Insets(10));
+        GridPane.setMargin(pokeImagecontainer, new Insets(10));
     }
 }
--- src/main/java/org/nice/pokedexxfx/components/PokemonList.java
+++ src/main/java/org/nice/pokedexxfx/components/PokemonList.java
@@ -1,10 +1,18 @@
 package org.nice.pokedexxfx.components;
 
 import javafx.application.Platform;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import org.nice.pokedexxfx.components.reusable.DynamicVBox;
 import org.nice.pokedexxfx.models.PokemonType;
+import org.nice.pokedexxfx.services.PokemonImage;
 import org.nice.pokedexxfx.services.PokemonService;
 import org.nice.pokedexxfx.services.SearchService;
 import rx.Observable;
@@ -12,30 +20,63 @@ import rx.Observable;
 import java.util.List;
 import java.util.Optional;
 
-public class PokemonList extends ScrollPane  {
+public class PokemonList extends ScrollPane {
     public PokemonList() {
 
         var listView = new DynamicVBox<>(
-            PokemonService.getInstance().filteredPokemonList,
-            pokemon -> String.valueOf(pokemon.id()),
-            pokemon -> {
-                var t = new Text(pokemon.name());
-                t.setOnMouseClicked(_ -> {
-                    PokemonService.getInstance().setCurrentPokemon(pokemon);
-                });
-                return t;
-            },
-            new Text("No pokemons found ;(")
-        );
+                PokemonService.getInstance().filteredPokemonList,
+                p -> String.valueOf(p.id()),
+                p -> {
+                    // main container
+                    var listBox = new HBox();
+                    listBox.setMinHeight(68);
+                    listBox.setPrefHeight(68);
+                    listBox.setMaxHeight(68);
+                    listBox.setSpacing(10);
+                    listBox.setPadding(new javafx.geometry.Insets(10));
+                    listBox.setStyle("-fx-border-radius: 10;");
+
+                    // id
+                    var idPanel = new HBox();
+                    var idLabel = new Label(String.valueOf(p.id()));
+                    idLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+                    // TODO: set color
+                    listBox.getChildren().add(idLabel);
+
+                    // name
+                    var pokeName = new Label(p.name());
+                    pokeName.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+                    pokeName.setTextFill(Color.WHITE);
+                    listBox.getChildren().add(pokeName);
+
+                    // sprite
+                    Image image = PokemonImage.getSprite(p);
+                    var sprite = new ImageView(image);
+                    sprite.setFitHeight(50);
+                    sprite.setFitWidth(50);
+                    listBox.getChildren().add(sprite);
+
+                    listBox.setOnMouseClicked(tite -> {
+                        PokemonService.getInstance().setCurrentPokemon(p);
+                    });
+
+                    return listBox;
+                },
+                new Text("No pokemons found ;("));
 
         setContent(listView);
         var service = SearchService.getInstance();
 
-        Observable.combineLatest(service.onSearchStringChange(), service.onTypeFilterChange(), List::of).subscribe(v -> {
-            var filters = (List<PokemonType>)v.get(1);
-            Platform.runLater(() -> PokemonService.getInstance().filterPokemons(filters, Optional.of(v.get(0).toString())));
-        });
+        Observable.combineLatest(service.onSearchStringChange(), service.onTypeFilterChange(), List::of)
+                .subscribe(v -> {
+                    if (v.get(0) instanceof List) {
+                        Platform.runLater(
+                                () -> PokemonService.getInstance().filterPokemons(List.of(), Optional.empty()));
+                        var filters = (List<PokemonType>) v.get(1);
+                        Platform.runLater(() -> PokemonService.getInstance().filterPokemons(filters,
+                                Optional.of(v.get(0).toString())));
+                    }
+                });
     }
-
 
 }
